86af8221dbe538623d52ef3c84fbc417174af5b0
unknown <filipegomes012642@gmail.com>
2024-03-06 18:05:10 -0300

    feat: configrando o cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import 'dotenv/config'
 import express from 'express'
 import rotas from './rotas'
+import cors from 'cors'
 
 const app = express()
 
+
+app.use(cors())
 app.use(express.json())
 app.use(rotas)
 
